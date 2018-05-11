19a769fca8eafb17d0369fe943f7d2d5e27f9c80
Kevin Cherrington <kevcherrington@gmail.com>
2018-05-11 09:32:24 -0600

    I fixed a bug where the welcome page wasn't getting shown because I hadn't added the annotations to the OAuthWebSecurityConfigurer class.

--- src/main/java/com/hostilesquirrel/config/OAuthWebSecurityConfigurer.java
+++ src/main/java/com/hostilesquirrel/config/OAuthWebSecurityConfigurer.java
@@ -1,8 +1,12 @@
 package com.hostilesquirrel.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
 
+@Component
+@EnableOAuth2Sso
 public class OAuthWebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
